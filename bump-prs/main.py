2c0f472c51a1f0e29cba497b20f5c279085c2c53
ztarbug <markus.zarbock@starwit>
2025-08-31 16:44:24 +0200

    add script to tabulate open PRs

---
+++ bump-prs/main.py
@@ -0,0 +1,55 @@
+import os
+import requests
+from dotenv import load_dotenv
+from tabulate import tabulate
+
+github_url="https://api.github.com"
+orgname="starwit"
+author="dependabot[bot]"
+
+def list_prs():
+    result = {}
+    url=github_url + "/search/issues?q=org:" + orgname + "+type:pr+state:open"
+    headers = {}
+    headers["Accept"] = "application/vnd.github+json"
+    if(TOKEN):
+        headers["Authorization"] = "Bearer " + TOKEN
+    resp = requests.get(url, headers=headers)
+    data = resp.json()
+    for pr in data["items"]:
+        title = pr["title"]
+        author = pr["user"]["login"]
+        if author not in result:
+            result[author] = []
+        result[author].append({
+            "title" : title,
+            "url" : pr["repository_url"],
+            "updated_at" : pr["updated_at"]
+        })
+    return result
+
+def output_as_table(data):
+    rows = []
+    for author, prs in data.items():
+        for pr in prs:
+            rows.append([
+                author,
+                pr["title"],
+                pr["url"].split("/")[-1],  # repo name from URL
+                pr["updated_at"]
+            ])
+
+    table = tabulate(rows, headers=["Author", "Title", "Repo", "Updated At"], tablefmt="github")
+    print(table)
+
+def main():
+    load_dotenv()
+    global TOKEN
+    TOKEN = os.getenv('TOKEN')
+    pr_list = list_prs()
+    output_as_table(pr_list)
+
+
+if __name__ == "__main__":
+    main()
+
